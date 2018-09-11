980553c194b981e8c8467d5ec555167ab2d5a7b2
Gabe Oleinik <gomusic14@gmail.com>
2018-09-11 16:42:47 -0400

    CONSOLE LOGS WORKING BABY

--- public/main.js
+++ public/main.js
@@ -1,5 +1,29 @@
-const main = () => {
-  document.querySelector('h1').textContent += '?'
-}
+const addOneTeamOne = event => {
+  console.log("FUGG YEAH DUGG");
+};
 
-document.addEventListener('DOMContentLoaded', main)
+const addOneTeamTwo = event => {
+  console.log("THAS FUGGIN SIGG");
+};
+const subtractOneTeamOne = event => {
+  console.log("FUGG MAN");
+};
+const subtractOneTeamTwo = event => {
+  console.log("FUGG");
+};
+
+const main = () => {
+  const addButtonTeamOne = document.querySelector(".team-1 .add");
+  addButtonTeamOne.addEventListener("click", addOneTeamOne);
+
+  const addButtonTeamTwo = document.querySelector(".team-2 .add");
+  addButtonTeamTwo.addEventListener("click", addOneTeamTwo);
+
+  const subtractButtonTeamOne = document.querySelector(".team-1 .subtract");
+  subtractButtonTeamOne.addEventListener("click", subtractOneTeamOne);
+
+  const subtractButtonTeamTwo = document.querySelector(".team-2 .subtract");
+  subtractButtonTeamTwo.addEventListener("click", subtractOneTeamTwo);
+};
+
+document.addEventListener("DOMContentLoaded", main);
